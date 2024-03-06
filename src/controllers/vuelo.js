c8f6a3649eb7d1849bbdb60ad6bda1e666e6c4fc
Hermmann Blanco Serrano <hermanalbertob@gmail.com>
2024-03-06 20:11:19 +0000

    buscar vuelos disponibles, hecho

--- src/controllers/vuelo.js
+++ src/controllers/vuelo.js
@@ -1,5 +1,49 @@
 const { supabase } = require("../supabase/client");
 
+const getAbleFlight = async (req, res) => {
+  try {
+    //const {disponible} = req.query;
+    let x = true;
+    const { data, error} = await supabase
+    .from('vuelos').select().eq('disponible', true);
+
+    
+    
+    if (error) {
+      throw error;   
+    }
+  
+    res.status(200).send(data);
+    
+  } catch (error) {
+    console.error('Error al obtener los vuelos:', error.message);
+    //res.status(500).json({message: "Error al obtener los vuelos"});
+    throw error;
+  }
+}
+
+const getFlights = async (req, res) => {
+    
+    try {
+        const { data, error } = await supabase
+          .from('vuelos')
+          .select('*');
+    
+          console.log(data);
+        if (error) {
+          throw error;
+          //res.status(400).json({message: "Error"});
+        }
+        //console.log("data", data);
+        res.status(200).send(data);
+        //return data;
+      } catch (error) {
+        console.error('Error al obtener los vuelos:', error.message);
+        res.status(500).json({message: "Error al obtener los vuelos"});
+        throw error;
+      }
+}
+
 const createFlight = async (req, res ) => {
     try {
       const {origen, destino, fecha_salida} = req.body;
@@ -22,4 +66,6 @@ const createFlight = async (req, res ) => {
 
   module.exports = {
     createFlight,
+    getFlights,
+    getAbleFlight,
   }
--- src/routes/vuelo.js
+++ src/routes/vuelo.js
@@ -2,7 +2,11 @@ const {Router} = require('express');
 const router = Router();
 
 const {createFlight} = require('../controllers/vuelo');
+const {getFlights} = require('../controllers/vuelo');
+const {getAbleFlight} = require('../controllers/vuelo');
 
 router.post('/vuelo', createFlight);
+router.get('/vuelo', getFlights);
+router.get('/vuelo/disponible', getAbleFlight);
 
 module.exports = router;
